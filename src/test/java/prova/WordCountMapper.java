b15485f9374ade280dd7024197714695df3196a9
stefanomaga <ste.maga@stud.uniroma3.it>
2015-07-07 15:50:08 +0200

    Word Count Ordinato

--- src/test/java/prova/WordCountMapper.java
+++ src/test/java/prova/WordCountMapper.java
@@ -13,19 +13,29 @@ public class WordCountMapper extends
 		Mapper<LongWritable, Text, Text, IntWritable> {
 
 	private static final IntWritable one = new IntWritable(1);
-	private Text word = new Text();
+	private Text product = new Text();
 
 	public void map(LongWritable key, Text value, Context context)
 			throws IOException, InterruptedException {
 
 		String line = value.toString();
-		StringTokenizer tokenizer = new StringTokenizer(line);
+		String allProducts = "";
+
+		StringTokenizer tokenizer = new StringTokenizer(line, "\n");
+		
+		String products = "";	
+		
+		while (tokenizer.hasMoreTokens()) {
+			products = tokenizer.nextToken().substring(11);
+			allProducts=allProducts+products+",";
+		}
+		
+		tokenizer = new StringTokenizer(allProducts, ",");
 
 		while (tokenizer.hasMoreTokens()) {
-			word.set(tokenizer.nextToken());
-			context.write(word, one);
+ 			product.set(tokenizer.nextToken());
+			context.write(product, one);
 		}
-
 	}
 
 }
